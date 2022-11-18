3562def6ad43e77108294ade92563132ef010119
Shubjaura <90023580+Shubjaura@users.noreply.github.com>
2022-11-17 22:26:56 -0700

    Add files via upload
    This script loads the in the database. 
    NOTE: The indexing is not done yet. So, it is possible that one has to manually create an index for the fields that they are working with. Use something like this "dblp.create_index([ ("authors", 1) ])" . This indexes the author's field. Once we know what fields we need to index and what format are going to use (ex: dblp.create_index([ ("authors", "text") ]), we can change our load.py file to perform indexing after loading the data.

---
+++ load.py
@@ -0,0 +1,27 @@
+import sys
+from pymongo import MongoClient
+from pymongo.collection import Collection
+import os
+
+
+def insert_data(filename, port):
+    cmd_str = f"mongoimport --port {port} --db 291db --collection dblp --drop --batchSize 15000 --file {filename}"
+    os.system(cmd_str)
+    client = MongoClient(f"mongodb://localhost:{port}")
+    db = client["291db"]
+    dblp = db["dblp"]
+
+
+def user_input():
+    filename = input('Please provided file name (include .json): ')
+    port = input('port: ')
+    return filename, port
+
+def main():
+    filename, port = user_input()
+    print(filename)
+    print(port)
+    insert_data(port,filename)
+
+if __name__ == "__main__":
+    main()
