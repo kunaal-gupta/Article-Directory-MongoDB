b41a2514ea77dba1c913a0cf9eb383cf22b33573
Kunaal Gupta <87749508+kunaal-gupta@users.noreply.github.com>
2022-11-21 21:56:16 -0700

    indexing and drop col

--- load.py
+++ load.py
@@ -1,4 +1,6 @@
 import sys
+
+import pymongo
 from pymongo import MongoClient
 from pymongo.collection import Collection
 import os
@@ -14,8 +16,20 @@ def insert_data(filename, port):
     cmd_str = f"mongoimport --port {port} --db 291db --collection dblp --drop --batchSize 15000 --file {filename}"
     os.system(cmd_str)
     client = MongoClient(f"mongodb://localhost:{port}")
-    mydb = client["291db"]
-    mycol = mydb["dblp"]
+    db = client["291db"]
+
+    collist = db.list_collection_names()
+    if 'dblp' in collist:
+        dblp.drop()
+
+    dblp = db["dblp"]
+
+    # Indices
+    dblp.update_many({}, [{'$set': {'year': {'$toString': '$year'}}}])  # Convert year to string
+    dblp.drop_indexes()
+    dblp.create_index([('title', pymongo.TEXT), ('authors', pymongo.TEXT), ('abstract', pymongo.TEXT), ('venue', pymongo.TEXT)
+        , ('year', pymongo.TEXT)], default_language="none")
+    dblp.create_index([('references', 1)])
 
 
 def main():
