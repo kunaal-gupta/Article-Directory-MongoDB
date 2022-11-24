50e4ab2c9d571768449545cad0d06542b1b56ac8
Kunaal Gupta <87749508+kunaal-gupta@users.noreply.github.com>
2022-11-24 12:02:51 -0700

    Update addArticle.py

--- addArticle.py
+++ addArticle.py
@@ -2,12 +2,17 @@ import pymongo
 from pymongo.collection import Collection
 
 
-def add_article(dblp:Collection):
+def add_an_article(dblp:Collection):
+    print('We are now going to ask you a few details to add an article.')
     """Collecting all Ids"""
     Ids = []
     for x in dblp.find({}, {'id': 1}):
         Ids.append(x['id'])
 
+    print('So let\'s begin..')
+    print()
+
+    print('------------------------------')
 
     """Getting Unique Ids from user"""
     UniqueId = input('UniqueId: ')
@@ -33,7 +38,7 @@ def add_article(dblp:Collection):
     Abstract = None
     References = []
     n_citation = 0
-
+    print()
     """Inserting document in the collection"""
     try:
         mydict = {'abstract': Abstract, 'authors': Authors, 'n_citation': n_citation, 'references': References, 'title': Title, 'venue': Venue, 'year': Year, 'id': UniqueId}
@@ -41,4 +46,8 @@ def add_article(dblp:Collection):
     except:
         pass
     else:
-        print('Successfully inserted document in the collection')
+        print('Successfully inserted a new article in the collection')
+    print('------------------------------')
+    print()
+
+
