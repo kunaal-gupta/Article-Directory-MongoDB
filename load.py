011254939bd2c533f7d3b76fef2ddbe7512d7712
Shubjaura <90023580+Shubjaura@users.noreply.github.com>
2022-11-23 18:54:17 -0700

    Update load.py

--- load.py
+++ load.py
@@ -4,39 +4,39 @@ import pymongo
 from pymongo import MongoClient
 from pymongo.collection import Collection
 import os
-
+import multiprocessing
+from datetime import datetime
 
 def user_input():
     filename = input('Please provided file name (include .json): ')
-    port = input('port: ')  # 271
+    port = input('port: ')  
     return filename, port
 
 
-def insert_data(filename, port):
-    cmd_str = f"mongoimport --port {port} --db 291db --collection dblp --drop --batchSize 15000 --file {filename}"
-    os.system(cmd_str)
+def insert_data(json_filename, port):
+    time_start=datetime.now()
+    mongoprint_cmd = f"mongoimport --port {port} --db 291db --collection dblp --drop --batchSize 15000 --file {json_filename} --numInsertionWorkers {multiprocessing.cpu_count()}"
+    os.system(mongoprint_cmd)
     client = MongoClient(f"mongodb://localhost:{port}")
     db = client["291db"]
-
-    collist = db.list_collection_names()
-    if 'dblp' in collist:
-        dblp.drop()
-
     dblp = db["dblp"]
+    dblp.drop_indexes()
+    dblp.create_index([('references', 1)])
 
     # Indices
     dblp.update_many({}, [{'$set': {'year': {'$toString': '$year'}}}])  # Convert year to string
-    dblp.drop_indexes()
-    dblp.create_index([('title', pymongo.TEXT), ('authors', pymongo.TEXT), ('abstract', pymongo.TEXT), ('venue', pymongo.TEXT)
-        , ('year', pymongo.TEXT)], default_language="none")
-    dblp.create_index([('references', 1)])
+    dblp.create_index([('title', pymongo.TEXT), ('authors', pymongo.TEXT), ('abstract', pymongo.TEXT), ('venue', pymongo.TEXT), ('year', pymongo.TEXT)], default_language="none")
+
+    time_end=datetime.now()
+    e= time_end - time_start
+    print("The execution time of python program is : ",e)
+
+    print("Collection is Created!")
 
 
 def main():
-    filename, port = user_input()
-    print(filename)
-    print(port)
-    insert_data(filename, port)
+    json_filename, port = user_input()
+    insert_data(json_filename, port)
 
 
 if __name__ == "__main__":
